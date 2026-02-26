2d72d119bbd8bfba13a2655bfa540ed7af306291
fantomeagle77-tech <fantomeagle77@gmail.com>
2026-02-26 21:50:17 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE || "http://127.0.0.1:8000";
+export const API_BASE = import.meta.env.VITE_API_BASE || "http://127.0.0.1:8000";
 
 async function request(path, options = {}) {
   const isFormData = options.body instanceof FormData;
