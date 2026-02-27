73b3e1b258bfdc2e05428916d2eb4500894893a2
fantomeagle77-tech <fantomeagle77@gmail.com>
2026-02-27 12:15:35 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE =
+export const API_BASE =
   (import.meta.env.VITE_API_BASE && import.meta.env.VITE_API_BASE.replace(/\/+$/, "")) ||
   "https://cityhelp-backend-gtpu.onrender.com";
 
