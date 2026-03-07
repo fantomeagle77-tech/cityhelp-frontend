946a6f5962a1b653f31ff1c90b878332c8c9f8d5
fantomeagle77-tech <fantomeagle77@gmail.com>
2026-03-07 22:38:27 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import App from "./App";
 import "./styles.css";
 import "leaflet/dist/leaflet.css";
 import "./leafletFix";
+import { HelmetProvider } from "react-helmet-async";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
